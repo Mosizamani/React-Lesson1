2fe425bb0da19da53043e0ddfbbde298552dd202
Mosi Zamani <84128703+MostafaZamanicivil@users.noreply.github.com>
2024-09-15 16:06:33 -0700

    new button added and some codes written in App file

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -2,20 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from '/src/button.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const music = {
+    title: 'Parvaz',
+    artist: 'Shadmehr'
+  }
+  const composer = {
+    name: 'Shadmehr Aghili',
+    age: 47,
+    live: 'Los Angeles'
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <p>
+        {music.title} by {music.artist}
+      </p>
+      <p>
+        composer: {composer.name} who is {composer.age} years old and lives in {composer.live}
+      </p>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -28,6 +37,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Button>Click here!</Button>
     </>
   )
 }
